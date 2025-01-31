5630667cb2eac28190a4ae4061f342d92d3c95e7
hjibran <haidirjibran@gmail.com>
2025-01-31 16:17:22 +0800

    Mengubah peta sebaran focmec sehingga warna kedalaman diplot dalam mode diskrit, setiap 10 km

--- generic-mapping-tools/script/01-sebaranfocmec.py
+++ generic-mapping-tools/script/01-sebaranfocmec.py
@@ -3,6 +3,7 @@
 
 # import module
 import pygmt
+import numpy as np
 import geopandas as gpd
 import pandas as pd
 
@@ -17,7 +18,11 @@ fig = pygmt.Figure()
 fig.grdimage(grid=grid, # call grid
              frame=["a", "EWSN"],#, "+tPulau Sulawesi"],
              projection="M10c",
-             cmap= "/mnt/d/celebes-stress-inversion-project/generic-mapping-tools/script/color.cpt")
+             cmap= 'gray'#"/mnt/d/celebes-stress-inversion-project/generic-mapping-tools/script/color.cpt"
+)
+fig.coast(shorelines='1p,black'
+
+)
 
 # display color bar from grid image
 #fig.colorbar(frame=["a3000f1000", "x+lElevation", "y+lm"],
@@ -33,42 +38,8 @@ fig.plot(data="/mnt/d/celebes-stress-inversion-project/data/batas-lempeng-dan-pa
          style="f1c/0.1c+l+t",
          fill="black")
 
-# plot focal mecanism
-fm = pd.read_csv("/mnt/d/celebes-stress-inversion-project/data/siap-olah/fm50km7mw.csv")
-FM = fm.iloc[:, [3, 2, 4, 6, 7, 8, 5]]
-FM.columns = ["longitude", "latitude", "depth", "strike", "dip", "rake", "magnitude"]
 
-
-#make color ramp for depth
-pygmt.makecpt(cmap="seis", reverse = True, series=[FM.depth.min(),FM.depth.max()])
-
-#Plot erathquake based on depth
-fig.plot(x = FM.longitude,
-         y = FM.latitude,
-         size = 0.001 * (2**FM.magnitude),
-         pen ="faint",
-         style ="c0.15",
-         fill =FM.depth,
-         cmap = True)
-
-
-#Plot Data Focal Mechanisme
-fig.meca(spec=FM,
-         cmap = True,
-         pen="0.3p,black,solid",
-         scale ="0.3c",
-         frame = True)
-
-fig.basemap(rose="jTL+w0.9c+lW,E,S,N+o0.3c/0.3c+f2",
-             #map_scale="jTL+w200k+o0.45c/0.5c+f+lkm",
-             ) 
-#fig.legend(position="jTL+o0.2/1.6c", box=True)
-
-#Plot colorbar
-#fig.colorbar(frame=["x+lDepth (km)"],#"af+l'Depth (km)'", 
-#             position="JMR+w-10/0.5+o3/0c+mn+")#+w-3/0.25+o0.3/0.3c+mn+")
-
-fill = "red"
+fill = "black"
 fig.plot(x=119.4, y=-5.16, style="s0.175c", fill=fill, pen="1p,black")
 fig.plot(x=119.8, y=-0.90, style="s0.175c", fill=fill, pen="1p,black")
 fig.plot(x=118.9, y=-2.68, style="s0.175c", fill=fill, pen="1p,black")
@@ -90,9 +61,71 @@ fig.plot(x=[122.65, 123.1], y=[0, 0.54], pen="0.75p,black")
 fig.text(x=124.7, y=2, text="Manado", font=font)
 fig.plot(x=[124.7, 124.8], y=[1.88, 1.49], pen="0.75p,black")
 
+# plot focal mecanism
+fm = pd.read_csv("/mnt/d/celebes-stress-inversion-project/data/siap-olah/fm50km7mw.csv")
+FM = fm.iloc[:, [3, 2, 4, 6, 7, 8, 5]]
+FM.columns = ["longitude", "latitude", "depth", "strike", "dip", "rake", "magnitude"]
+
+
+#make color ramp for depth
+#depth_group = []
+#for i in range(len(FM)):
+#    if 0 <= FM['depth'][i] < 10:
+#        group = '0-10'
+#    elif 10 <= FM['depth'][i] < 20:
+#        group = '10-20'
+#    elif 20 <= FM['depth'][i] < 30:
+#        group = '20-30'
+#    elif 30 <= FM['depth'][i] < 40:
+#        group = '30-40'
+#    elif 40 <= FM['depth'][i] <= 50:
+#        group = '40-50'
+#    depth_group.append(group)
+#depth_group = pd.DataFrame(np.array(depth_group), columns=['group'])
+
+pygmt.makecpt(cmap="seis", 
+              reverse = True, 
+              series=[FM.depth.min(),FM.depth.max(), 10],
+              #color_model="+c0-10,10-20,20-30,30-40,40-50",
+)
+#Plot erathquake based on depth
+fig.plot(x = FM.longitude,
+         y = FM.latitude,
+         size = 0.001 * (2**FM.magnitude),
+         pen ="faint",
+         style ="c0.15",
+         fill = FM.depth,
+         cmap = True)
+
+#Plot Data Focal Mechanisme
+fig.meca(spec=FM,
+         cmap = True,
+         pen="0.2p,black,solid",
+         scale ="0.2c",
+         frame = True)
+
+#fig.colorbar(
+#    cmap=True,  # Use colormap set up above
+#    # Colorbar placed inside the plot bounding box (j) in the Bottom Left (BL) corner,
+#    # with an offset (+o) by 0.5 cm horizontally and 0.8 cm vertically from the anchor
+#    # point, and plotted horizontally (+h)
+#    position="JBL+o0.5c/0.4c+w-10c/1.75c",
+#    #box='white',
+#    # Divide colorbar into equal-sized rectangles
+#    equalsize=0.5,
+#)
+
+fig.basemap(rose="jTL+w0.9c+lW,E,S,N+o0.3c/0.3c+f2",
+             #map_scale="jTL+w200k+o0.45c/0.5c+f+lkm",
+             ) 
+#fig.legend(position="jTL+o0.2/1.6c", box=True)
+
+#Plot colorbar
+#fig.colorbar(frame=["x+lDepth (km)"],#"af+l'Depth (km)'", 
+#             position="JMR+w-10/0.5+o3/0c+mn+")#+w-3/0.25+o0.3/0.3c+mn+")
+
 # show picture
 fig.show()
-
 #fig.savefig("/mnt/d/celebes-stress-inversion-project/generic-mapping-tools/maps/basemap.pdf")
 #plate_boundary = gpd.read_file("/mnt/d/celebes-stress-inversion-project/data/batas-lempeng-dan-patahan/bird2003/PB2002_steps.shp")
 #fig.plot(data=plate_boundary, pen="0.25p,black,-", label="Plate Boundary")
