02b4f4df81a3d9f1903f0e43910882b41f79e194
hjibran <haidirjibran@gmail.com>
2025-04-21 17:05:35 +0800

    membuat ppt before after 2018 mw7.5 Palu

--- Stressinverse_1.1.3/Output/shmax/diskrit_shmax_type_palu.py
+++ Stressinverse_1.1.3/Output/shmax/diskrit_shmax_type_palu.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import os
 
-def shmax_plot(lon, lat, down, up, AR):
+# Peruntukan 'shmax' / 'sebaran data before' / 'sebaran data after
+untuk = 'sebaran data after'
+
+# Membuat fungsi plot shmax
+def shmax_plot(lon, lat, down, up, fill):
     if down <=180:
         down1 = down+180
     elif down > 180:
@@ -27,16 +32,18 @@ def shmax_plot(lon, lat, down, up, AR):
     else:
         fill = "darkred"
     '''
+    """ 
     if 0 <= AR < 1:
         fill = "blue"
     elif 1 <= AR < 2:
         fill = "green"
     elif 2.0 <= AR < 3:
-        fill = "red"
+        fill = "red" """
     
     data = [[lon, lat, 360-up+90, 360-down+90], [lon, lat, 360-up1+90, 360-down1+90]]
     fig.plot(data=data, style="w3c", fill=fill, cmap=False, transparency=25, pen="black")
 
+# Memasukkan data hasil inversi before after
 table = pd.read_csv("/mnt/d/celebes-stress-inversion-project/Stressinverse_1.1.3/Output/shmax/shmax_before_after_palu.csv", comment='#')
 
 table1 = table#table.query('name == 15.40')
@@ -45,32 +52,45 @@ table1 = table1.reset_index()
 FM = table1.iloc[:, [2, 3, 4, 5, 6, 7, 11]]
 FM.columns = ["longitude", "latitude", "depth", "strike", "dip", "rake", "magnitude"]
 
-import pygmt
+# Memasukkan data sebaran kejadian gempa sebelum dan setelah Gempa palu 2018
+databefore = pd.read_csv('/mnt/d/celebes-stress-inversion-project/Result/eps0.40min15-final/clustered/cls7-before-palu.csv')
+FMbefore = databefore.iloc[:, [3, 2, 4, 6, 7, 8, 5]]
+FMbefore.columns = ["longitude", "latitude", "depth", "strike", "dip", "rake", "magnitude"]
+dataafter = pd.read_csv('/mnt/d/celebes-stress-inversion-project/Result/eps0.40min15-final/clustered/cls7-after-palu.csv')
+FMafter = dataafter.iloc[:, [3, 2, 4, 6, 7, 8, 5]]
+FMafter.columns = ["longitude", "latitude", "depth", "strike", "dip", "rake", "magnitude"]
 
+# Mulai melakukan plot
+import pygmt
 fig = pygmt.Figure()
 
+# Mengimport data relief
 grid = pygmt.datasets.load_earth_relief(resolution="30s", # resolution of earth relief
                                         registration="gridline",
-                                        region=[120.12075806451612-1, 120.12075806451612+1, -1.1031451612903227-1, -1.1031451612903227+1],
+                                        region=[120.12075806451612-1.5, 120.12075806451612+1.5, -1.1031451612903227-1.5, -1.1031451612903227+1.5],
                                         )
-
+# Plot data relief
 fig.grdimage(grid=grid,
              projection='M10c',
              frame=["a2", "EWSN"],
              cmap= "/mnt/d/celebes-stress-inversion-project/generic-mapping-tools/script/color.cpt")
 
-fig.coast(region=[120.12075806451612-1, 120.12075806451612+1, -1.1031451612903227-1, -1.1031451612903227+1],
+# Menimpah data relief dengan laut berwarna putih
+fig.coast(region=[120.12075806451612-1.5, 120.12075806451612+1.5, -1.1031451612903227-1.5, -1.1031451612903227+1.5],
           #land='lightgray',
           resolution='f',
           water='white'
           #shorelines='0.25p,black,solid'
           )
 
+# Input data patahan
 fig.plot(
     data="/mnt/d/celebes-stress-inversion-project/data/batas-lempeng-dan-patahan/indonesiafaults.gmt", 
     pen="0.6p,dimgray",
     label="Patahan"
 )
+
+# Input data Trench
 fig.plot(
     data="/mnt/d/celebes-stress-inversion-project/data/batas-lempeng-dan-patahan/trench.gmt", 
     pen="0.8p,dimgray", 
@@ -78,6 +98,48 @@ fig.plot(
     fill="dimgray",
     label="Subduksi"
 )
+
+# Input data buffer cluster palu
+bufferpalu = gpd.read_file("/mnt/d/celebes-stress-inversion-project/Result/eps0.40min15-final/eps0.40pts15-Palu.shp")
+fig.plot(data=bufferpalu, pen="1.5p,black,--")#, label="ε=0.50 MinPts=15")
+
+
+if untuk == 'sebaran data before':
+    focmec = FMbefore
+elif untuk == 'sebaran data after':
+    focmec = FMafter
+
+if untuk != 'shmax':
+    '''
+    # Membuat cmap untuk plot kedalaman
+    pygmt.makecpt(cmap="seis", 
+                  reverse = True, 
+                  series=[focmec.depth.min(),focmec.depth.max(), 10],
+                  #color_model="+c0-10,10-20,20-30,30-40,40-50",
+    )
+
+    #Plot erathquake based on depth
+    fig.plot(x = focmec.longitude,
+             y = focmec.latitude,
+             size = 0.001 * (2**focmec.magnitude),
+             pen ="faint",
+             style ="c0.15",
+             fill = focmec.depth,
+             cmap = True) '''
+
+    #Plot Data Focal Mechanisme
+    fig.meca(spec=focmec,
+             compressionfill='black', # cmap = True,
+             pen="0.5p,black,solid",
+             scale ="0.25c",
+             frame = True)
+    
+    fig.meca(spec=FMbefore,
+             compressionfill='red', # cmap = True,
+             pen="0.5p,black,solid",
+             scale ="0.25c",
+             frame = True)
+    
 
 #df = pd.read_csv("/mnt/d/plate-motion-itrf96-project-location.csv")
 #
@@ -129,13 +191,18 @@ fig.plot(
 #    font="gold2"
 #)
 
+""" # Membuat gpt untuk warna shmax sesuai dengan regime
 os.system('gmt makecpt -Cdarkblue,blue,green,red,darkred -T0/3/0.02 -Z > /mnt/d/celebes-stress-inversion-project/Stressinverse_1.1.3/Output/shmax/custom.cpt')
 sebaran = np.array([0,3])
 pygmt.makecpt(cmap="/mnt/d/celebes-stress-inversion-project/Stressinverse_1.1.3/Output/shmax/custom.cpt", 
-              series=[sebaran.min(), sebaran.max()])
+              series=[sebaran.min(), sebaran.max()]) """
 
-for i in range(len(table1)):
-    shmax_plot(table1['lon'][i], table1['lat'][i], table1['down'][i], table1['up'][i], table1['AR'][i])
+
+# Plot Shmax
+if untuk == 'shmax':
+    color= ['red','black']
+    for i in range(len(table1)):
+        shmax_plot(table1['lon'][i], table1['lat'][i], table1['down'][i], table1['up'][i], color[i])
 
 fig.basemap(rose="jBL+w1.2c+lW,E,S,N+o0.1c/0.1c+f2") 
 
